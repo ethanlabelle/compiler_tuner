cd5c7d87da5c6eae5ec460430bf0a07d5167e88a
Ethan LaBelle <ethan.labelle01@gmail.com>
2022-12-21 15:21:34 -0500

    refactor

--- src/performance.c
+++ src/performance.c
@@ -5,6 +5,21 @@
 #include "matmul.h"
 
 void benchmark() {
+    int size = 1024;
+    int elms = size * size;
+    int *A = calloc(elms, sizeof(int));
+    int *B = calloc(elms, sizeof(int));
+    int *C = calloc(elms, sizeof(int));
+    setRand(A, size);
+    setRand(B, size);
+    float startTime = (float)clock()/CLOCKS_PER_SEC;
+    matmul(A, B, C, size);
+    float endTime = (float)clock()/CLOCKS_PER_SEC;
+    float timeElapsed = endTime - startTime;
+    printf("time: %fs\n", timeElapsed);
+}
+
+void benchmark2() {
     int size = 2048;
     int elms = size * size;
     int *A = calloc(elms, sizeof(int));
