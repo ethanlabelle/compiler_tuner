3d6bb54f7f996c9833373fef5bf1dd1829b25d43
Ethan LaBelle <ethan.labelle01@gmail.com>
2023-02-27 00:23:50 -0500

    analysis

--- analysis.py
+++ analysis.py
@@ -1,17 +1,63 @@
 import matplotlib.pyplot as plt
 
-times = []
+# times=[]
+# with open("mac_results.txt", "r") as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         if line.startswith("CompletedProcess(args=['./driver', '-b'], returncode=0, stdout='time:"):
+#             tokens = line.split("time: ")
+#             time = tokens[1].split("s")[0]
+#             times.append(float(time))
 
-with open("mac_results.txt", "r") as f:
-    lines = f.readlines()
-    for line in lines:
-        if line.startswith("CompletedProcess(args=['./driver', '-b'], returncode=0, stdout='time:"):
-            tokens = line.split("time: ")
-            time = tokens[1].split("s")[0]
-            times.append(float(time))
+#     times = sorted(times, reverse=True)
+#     print(times)
 
-    times = sorted(times, reverse=True)
-    print(times)
+# plt.plot(times)
+# plt.show()
 
-plt.plot(times)
-plt.show()
+# times=[]
+# with open("intel_results.txt", "r") as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         if line.startswith("CompletedProcess(args=['./driver', '-b'], returncode=0, stdout='time:"):
+#             tokens = line.split("time: ")
+#             time = tokens[1].split("s")[0]
+#             times.append(float(time))
+
+#     times = sorted(times, reverse=True)
+#     print(times)
+
+# plt.plot(times)
+# plt.show()
+
+# with open("mac_iterations.txt", "r") as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         if line.startswith("CompletedProcess(args=['./driver', '-b'], returncode=0, stdout="):
+#             test = []
+#             tokens = line.split("stdout='")
+#             trials = tokens[1]
+#             trials = trials.split('time ')
+#             for trial in trials:
+#                 if len(trial) > 0:
+#                     trial = trial.split(": ")
+#                     trial = trial[1].split("s")[0]
+#                     test.append(float(trial))
+#             plt.plot(test)
+# plt.show()
+
+# with open("intel_iterations.txt", "r") as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         if line.startswith("CompletedProcess(args=['./driver', '-b'], returncode=0, stdout="):
+#             test = []
+#             tokens = line.split("stdout='")
+#             trials = tokens[1]
+#             trials = trials.split('time ')
+#             for trial in trials:
+#                 if len(trial) > 0:
+#                     trial = trial.split(": ")
+#                     trial = trial[1].split("s")[0]
+#                     test.append(float(trial))
+#             plt.plot(test)
+# plt.show()
